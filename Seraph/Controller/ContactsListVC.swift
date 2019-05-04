a8e8fe17ee52108379bfa81f94795a6a86e6d8c1
mmah65 <mmah65@student.monash.edu>
2019-05-04 13:14:35 +1000

    Added database listener code to contacts list vc

--- Seraph/Controller/ContactsListVC.swift
+++ Seraph/Controller/ContactsListVC.swift
@@ -8,24 +8,46 @@
 
 import UIKit
 
-class ContactsListVC: UITableViewController {
+class ContactsListVC: UITableViewController, DatabaseListener {
     
-    var contacts = [Contact]()
+    var contactsList = [Contact]()
+    weak var databaseController: DatabaseProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Setup database controller
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        databaseController = appDelegate.databaseController
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return contacts.count
+        return contactsList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
-        let contact = contacts[indexPath.row]
+        let contact = contactsList[indexPath.row]
         
         cell.textLabel?.text = contact.name
         return cell
+    }
+    
+    var listenerType: ListenerType = ListenerType.contacts
+    
+    func onContactsListChange(change: DatabaseChange, contacts: [Contact]) {
+        contactsList = contacts
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        databaseController?.addListener(listener: self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        databaseController?.removeListener(listener: self)
     }
     
 }
