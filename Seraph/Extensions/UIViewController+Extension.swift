da570c5674cc9501eb263514120dffd15b5fcd7f
mmah65 <mmah65@student.monash.edu>
2019-05-05 14:52:05 +1000

    Customized alert to have dark background

--- Seraph/Extensions/UIViewController+Extension.swift
+++ Seraph/Extensions/UIViewController+Extension.swift
@@ -13,6 +13,11 @@ extension UIViewController {
     func displayMessage(title: String, message: String, shouldPopViewControllerOnCompletion: Bool) {
         // display alert with the specified title and message
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+        subview.backgroundColor = UIColor(red: 35/255, green: 35/255, blue: 35/255, alpha: 1)
+        alert.setValue(NSAttributedString(string: title, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17),NSAttributedString.Key.foregroundColor : UIColor.white]), forKey: "attributedTitle")
+        alert.setValue(NSAttributedString(string: message, attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14),NSAttributedString.Key.foregroundColor : UIColor.lightGray]), forKey: "attributedMessage")
+        alert.view.tintColor = UIColor(red: 85/255, green: 186/255, blue: 85/255, alpha: 1)
         // pop current view controller on dismissing alert message
         if shouldPopViewControllerOnCompletion {
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (_) in
