371a421b4923cd44f4c48fa669aac2d638fd3285
mmah65 <mmah65@student.monash.edu>
2019-05-05 16:47:21 +1000

    Fixed clear button in textfields

---
+++ Seraph/View/TintedTextField.swift
@@ -0,0 +1,75 @@
+//
+//  BlackTextField.swift
+//  Seraph
+//
+//  Created by Musa  Mahmud on 5/5/19.
+//  Copyright © 2019 Mubtasim  Mahmud. All rights reserved.
+//
+
+import UIKit
+
+class TintTextField: UITextField {
+    
+    var tintedClearImage: UIImage?
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        setupTintColor()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTintColor()
+    }
+    
+    func setupTintColor() {
+        clearButtonMode = UITextField.ViewMode.always
+        layer.masksToBounds = true
+        backgroundColor = UIColor(red: 28/255, green: 28/255, blue: 28/255, alpha: 1)
+        textColor = UIColor.lightGray
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        tintClearImage()
+    }
+    
+    private func tintClearImage() {
+        for view in subviews as! [UIView] {
+            if view is UIButton {
+                let button = view as! UIButton
+                if let uiImage = button.image(for: .highlighted) {
+                    if tintedClearImage == nil {
+                        tintedClearImage = tintImage(image: uiImage, color: UIColor.lightGray)
+                    }
+                    button.setImage(tintedClearImage, for: .normal)
+                    button.setImage(tintedClearImage, for: .highlighted)
+                }
+            }
+        }
+    }
+}
+
+
+func tintImage(image: UIImage, color: UIColor) -> UIImage {
+    let size = image.size
+    
+    UIGraphicsBeginImageContextWithOptions(size, false, image.scale)
+    let context = UIGraphicsGetCurrentContext()
+    image.draw(at: CGPoint(x: 0, y: 0), blendMode: CGBlendMode.normal, alpha: 1.0)
+    
+    context!.setFillColor(color.cgColor)
+    context!.setBlendMode(CGBlendMode.sourceIn)
+    context!.setAlpha(1.0)
+    
+    let rect = CGRect(
+        x: 0,
+        y: 0,
+        width: image.size.width,
+        height: image.size.height)
+    UIGraphicsGetCurrentContext()!.fill(rect)
+    let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return tintedImage!
+}
